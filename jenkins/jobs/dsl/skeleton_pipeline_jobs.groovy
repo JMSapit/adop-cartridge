517ecbfad1cb9936c48d124fd618ebc5fbea3fd9
Jona Micah Fidel <jonavfidel@gmail.com>
2018-07-16 16:37:09 +0800

    update

--- jenkins/jobs/dsl/skeleton_pipeline_jobs.groovy
+++ jenkins/jobs/dsl/skeleton_pipeline_jobs.groovy
@@ -23,3 +23,45 @@ pipelineView.with{
     refreshFrequency(5)
 }
 
+cartridge_build.with{
+
+
+  scm {
+    git {           
+      remote {
+        credentials('adopadmin')
+        //url('https://jona.micah.v.fidel@innersource.accenture.com/scm/~jona.micah.v.fidel/devops_cps_forked.git')
+        url('http://34.201.189.209/gitlab/adopadmin/DevOps-OracleEBS.git')
+        }
+      branch('*/master')
+    }
+  }
+
+  wrappers {
+    preBuildCleanup()
+  }
+
+  triggers {
+    gitlabPush {
+      buildOnMergeRequestEvents(true)
+      buildOnPushEvents(true)
+      enableCiSkip(true)
+      setBuildDescription(false)
+      rebuildOpenMergeRequest('never')
+    }
+  
+    //goals('package')
+
+    steps {
+    shell('''
+mkdir target
+tar --exclude='./.git' --exclude='./target' -zcvf ./target/sample.tar .''')
+    }
+    publishers{
+    	downstream('<name of next job>','SUCCESS') 
+    }
+
+   
+  }
+}
+
